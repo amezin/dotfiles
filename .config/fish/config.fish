deab39c957646360dff5269238245fb5b0fe455b
Aleksandr Mezin <mezin.alexander@gmail.com>
2017-12-02 13:38:28 +0600

    Get rid of oh-my-fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,31 @@
+set -gx EDITOR nano
+
+if test -d "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+    set PATH $PATH "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+end
+
 set -l current_script (realpath (status -f))
 set -l current_script_dir (realpath (dirname "$current_script"))
 set -l root_dir (realpath "$current_script_dir/../..")
 
-set -gx OMF_PATH (realpath "$root_dir/oh-my-fish")
-set -gx OMF_CONFIG (realpath "$root_dir/config/omf")
+set -x PYTHONPATH (string join ":" "$PYTHONPATH" "$root_dir/virtualenv" "$root_dir/virtualfish")
+eval (python -m virtualfish auto_activation)
 
-source $OMF_PATH/init.fish
+set PATH $PATH "$root_dir/pyenv/bin" $root_dir/pyenv/plugins/*/bin
+status --is-interactive; and source (pyenv init -|psub)
+
+set -g theme_powerline_fonts no
+set -g theme_title_display_process yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_git_worktree_support yes
+set -g theme_display_user yes
+set -g theme_display_cmd_duration yes
+set -g theme_show_exit_status yes
+
+for theme_func in "$root_dir/theme-bobthefish"/*.fish
+    source "$theme_func"
+end
+
+if set -q ITERM_SESSION_ID
+    source "$root_dir/iterm2_shell_integration.fish"
+end
