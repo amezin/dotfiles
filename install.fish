87e01523133d9d45dd10c96edc9b4a2a578ca7f9
Aleksandr Mezin <mezin.alexander@gmail.com>
2019-11-05 00:46:52 +0600

    Replace gitconfig symlink with include

--- install.fish
+++ install.fish
@@ -11,4 +11,11 @@ end
 mkdir -p "$HOME/.config/fish/conf.d"
 ln -sf "$root_dir/config.fish" "$HOME/.config/fish/conf.d/dotfiles.fish"
 
-ln -sf "$root_dir/gitconfig" "$HOME/.gitconfig"
+set -l gitconfig_link (readlink -f "$HOME/.gitconfig")
+if test "$gitconfig_link" = "$root_dir/gitconfig"
+  rm "$HOME/.gitconfig"
+end
+
+if not git config --global --get include.path "$root_dir/gitconfig" >/dev/null
+  git config --global --add include.path "$root_dir/gitconfig"
+end
